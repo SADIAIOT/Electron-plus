09ac253f23d71a3556dde8fb02bce14ab0712ea0
Walter Alexandre Santana <162894298+mr-body@users.noreply.github.com>
2024-10-29 13:38:25 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    index: './src/index.jsx', // Entrada principal
-    form: './src/form.jsx',  // Entrada para o formulário
+    index: './src/index.jsx', // Main entry point
+    form: './src/form.jsx',   // Entry point for the form
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js', // Saída dinâmica: main.js para index.js e form.js para form.js
+    filename: '[name].js', // Dynamic output: index.js for index and form.js for form
   },
   module: {
     rules: [
@@ -18,6 +18,18 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/, // Rule to handle CSS files
+        use: ['style-loader', 'css-loader'], // Loaders for CSS
+      },
+      {
+        test: /\.scss$/, // Rule to handle SCSS files
+        use: [
+          'style-loader', // Injects styles into the DOM
+          'css-loader',   // Translates CSS into CommonJS
+          'sass-loader',  // Compiles Sass to CSS
+        ],
+      },
     ],
   },
   resolve: {
@@ -25,3 +37,4 @@ module.exports = {
   },
   target: 'electron-renderer',
 };
+  
